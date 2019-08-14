e7b180ddd18ce9104d674e7c555e896c25beaec2
Derek Etman <dereketman@gmail.com>
2019-08-14 16:42:00 -0700

    Now contains 2 components!!!!!

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React,{useState, useEffect} from "react";
 import axios from "axios";
 import "./App.css";
 import SpaceImgCard from "./components/imageComponents/spaceImgCard";
-import SpaceImgGrid from "./components/imageComponents/spaceImgGrid";
+import TextCard from "./components/textComponents/textCard";
+// import SpaceImgGrid from "./components/imageComponents/spaceImgGrid";
 
 function App() {
   const [nasaData,setNasaData] = useState([]);
@@ -20,15 +21,24 @@ function App() {
   return (
     <div className="App">
       <div className="nasaCardContainer">
+
         <div className="imgContainer">
-          <h1 className="appTitle">Nasa Photo of the Day!</h1>
+
+          <h1 className="appTitle">SPACE PHOTOS!</h1>
             <SpaceImgCard 
-            title={nasaData.title}
+             url={nasaData.url}
             media_type={nasaData.media_type}
-            date={nasaData.date}
-            url={nasaData.url}
-            description={nasaData.explanation}
-            />
+             />
+
+        </div>
+
+        <div className="textContainer">
+
+          <TextCard 
+          title={nasaData.title}
+          date={nasaData.date}
+          description={nasaData.explanation}/>
+          
         </div>
           
       </div>
--- src/components/imageComponents/spaceImgCard.js
+++ src/components/imageComponents/spaceImgCard.js
@@ -5,9 +5,6 @@ const SpaceImgCard = props =>{
     return (
         <div className="nasaCard" key={props.id}>
             <img className="nasaImg" alt="random space" src={props.url} />
-            <h3 className="nasaTitle">{props.title}</h3>
-            <h4 className="nasaDate">{props.date}</h4>
-            <p className="nasaImgDesc">{props.description}</p>
         </div>
     )
 }
